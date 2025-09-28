884d08885b3779bc3d2b3263c34a972d5efc7165
below-1 <jordanmeta4677@gmail.com>
2025-09-28 08:53:55 +0800

    add new files

---
+++ src/components/CountdownSection.tsx
@@ -0,0 +1,7 @@
+export function CountdownSection() {
+  return (
+    <section id="CountdownSection">
+      
+    </section>
+  )
+}
---
+++ src/components/GallerySection.tsx
@@ -0,0 +1,7 @@
+export function GallerySection() {
+  return (
+    <section id="GallerySection">
+
+    </section>
+  )
+}
