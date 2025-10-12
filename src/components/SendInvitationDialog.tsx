3314744b2fa2eebe2e12609a42ee360fd47cac20
below-1 <jordanmeta4677@gmail.com>
2025-10-12 15:08:19 +0800

    adapt send link for IOS

--- src/components/SendInvitationDialog.tsx
+++ src/components/SendInvitationDialog.tsx
@@ -7,31 +7,61 @@ import { Input } from "./ui/input";
 import { ISendHistorySchema, SendHistorySchema } from "@/lib/schema";
 import { sendInvitation } from "@/lib/actions";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { useActionState, useMemo, useState } from "react";
+import { Spinner } from "./ui/spinner";
+import { Whatsapp } from "./icons/whatsapp";
+
+export function isSafari() {
+  return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+}
 
 
 export function SendInvitationDialog() {
+  const inSafari = useMemo(() => {
+    return isSafari()
+  }, [])
+  const [ open, setOpen ] = useState(false)
   const form = useForm<ISendHistorySchema>({
     resolver: zodResolver(SendHistorySchema),
     defaultValues: {
       name: ""
     },
   })
+  const [ sendLinkForIOS, setSendLinkForIOS ] = useState("")
+
+  const [ loading, setLoading ] = useState(false)
 
   const onSubmit = async (data: ISendHistorySchema) => {
+    setLoading(true)
     const fd = new FormData()
     fd.append("name", data.name)
 
-    const result = await sendInvitation(fd)
-    if (!result || !result.success) {
-      console.error("terjadi kesalahan");
-      return;
+    try {
+      const result = await sendInvitation(fd)
+      if (!result || !result.success) {
+        console.error("terjadi kesalahan");
+        return;
+      }
+  
+      if (isSafari()) {
+        setSendLinkForIOS(result.data?.url || "")
+      } else {
+        window.open(result.data?.url, "_blank")
+      }
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setLoading(false)
     }
-
-    window.open(result.data?.url, "_blank");
   }
   return (
     <Form {...form}>
-      <Dialog>
+      <Dialog 
+        open={open}
+        onOpenChange={o => {
+          setOpen(o)
+        }}
+      >
           <form
             onSubmit={form.handleSubmit(onSubmit)}
           >
@@ -65,19 +95,55 @@ export function SendInvitationDialog() {
               </div>
               <DialogFooter>
                 <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
+                  <Button 
+                    variant="outline"
+                    onClick={() => {
+                      setOpen(false)
+                      setSendLinkForIOS("")
+                    }}
+                  >Cancel</Button>
                 </DialogClose>
-                <Button
-                  onClick={async () => {
-                    console.log("here");
-                    const vals = form.getValues();
-                    await form.trigger();
-                    if (vals.name) {
-                      await onSubmit(vals);
-                    }
-                      // if (form.formState.isValid) onSubmit(); // Call the `onSubmit` function if the form is validated
-                  }}
-                >Kirim</Button>
+                {(inSafari && sendLinkForIOS)
+                  ? (
+                    <Button
+                      asChild
+                      className="bg-[#075E54] text-white"
+                      onClick={async () => {
+                        console.log("here");
+                        const vals = form.getValues();
+                        await form.trigger();
+                        if (vals.name) {
+                          await onSubmit(vals);
+                        }
+                          // if (form.formState.isValid) onSubmit(); // Call the `onSubmit` function if the form is validated
+                      }}
+                    >
+                      <a href={sendLinkForIOS} target="_blank">
+                        <Whatsapp className="size-5" />
+                        Kirim
+                      </a>
+                    </Button>
+                  )
+                  : (
+                    <Button
+                      onClick={async () => {
+                        console.log("here");
+                        const vals = form.getValues();
+                        await form.trigger();
+                        if (vals.name) {
+                          await onSubmit(vals);
+                        }
+                          // if (form.formState.isValid) onSubmit(); // Call the `onSubmit` function if the form is validated
+                      }}
+                    >
+                      {loading 
+                        ? <Spinner />
+                        : (inSafari ? "Dapatkan Link" : "Kirim")
+                      }
+                    </Button>
+                  )
+                }
+                
               </DialogFooter>
             </DialogContent>
           </form>
